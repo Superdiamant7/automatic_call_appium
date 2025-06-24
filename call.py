8961bca340ff62e1c19193ead646cc45fc85b5b6
Superdiamant7 <mathis.demonet@protonmail.com>
2025-06-24 22:43:39 +0200

    Resolved a little bug

--- call.py
+++ call.py
@@ -21,13 +21,16 @@ def wait_for_incoming_call(timeout):
     while time.time() - start < timeout:
         answer_call()
         time.sleep(1)
+        answer_call()
         # If call activity detected :
         if driver.current_activity == "com.android.incallui.call.InCallActivity":
+            answer_call()
             return True
     return False
 
 # Uses adb to answer call
 def answer_call():
+    # Uses the headphone answer call action to answer a call without any UI
     subprocess.run(["adb", "shell", "input", "keyevent", "KEYCODE_HEADSETHOOK"]) 
 
 # Uses adb to make a call
@@ -43,7 +46,7 @@ driver = create_driver()
 
 # Waits to have an incoming call for 10 seconds
 print("Waiting for call incoming...")
-if wait_for_incoming_call(10):
+if wait_for_incoming_call(60):
     print("Call incoming ! Answering call.")
     if driver.current_activity == "com.android.incallui.call.InCallActivity":
         print("Call successfully answered !")
@@ -51,4 +54,4 @@ else:
     print(f"No call incoming, calling {number_to_call}")
     call(driver, number_to_call)
  
-driver.quit()
+driver.quit() 
