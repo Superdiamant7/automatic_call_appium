408ecd8aeefdab75ba3fcc420730114adcfc3fdc
unknown <mathis.demonet@protonmail.com>
2025-06-24 13:55:45 +0200

    Added comments

--- call.py
+++ call.py
@@ -1,9 +1,11 @@
+# Imports necessary libraries
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from selenium.webdriver.support.ui import WebDriverWait
 import subprocess
 import time 
 
+# Initialises the appium driver
 def create_driver():
     options = UiAutomator2Options()
     options.platform_name = "Android"
@@ -13,18 +15,22 @@ def create_driver():
     driver = webdriver.Remote("http://localhost:4723", options=options)
     return driver
 
+# Waits "timeout" secs to have an incoming call 
 def wait_for_incoming_call(timeout):
     start = time.time()
     while time.time() - start < timeout:
         answer_call()
         time.sleep(1)
+        # If call activity detected :
         if driver.current_activity == "com.android.incallui.call.InCallActivity":
             return True
     return False
 
+# Uses adb to answer call
 def answer_call():
     subprocess.run(["adb", "shell", "input", "keyevent", "KEYCODE_HEADSETHOOK"]) 
 
+# Uses adb to make a call
 def call(driver, number):
     driver.execute_script("mobile: shell", {
         "command": "am",
@@ -35,6 +41,7 @@ number_to_call = "+010123456789" # Number to call automaticaly
 
 driver = create_driver()
 
+# Waits to have an incoming call for 10 seconds
 print("Waiting for call incoming...")
 if wait_for_incoming_call(10):
     print("Call incoming ! Answering call.")
